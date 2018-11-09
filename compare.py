0d1e35b64b98990adf0442bc7a57d4cb55e022d3
Kevin Sturgeon <ksturgeon@gmail.com>
2018-11-09 11:22:23 -0500

    compare script

---
+++ compare.py
@@ -0,0 +1,19 @@
+import jaydebeapi
+jdbc_driver="org.apache.drill.jdbc.Driver"
+driver_jar="/opt/mapr/drill/jars/jdbc-driver/drill-jdbc-all-1.11.0.jar"
+
+print("connecting")
+conn = jaydebeapi.connect(jdbc_driver,'jdbc:drill:zk=mapr-zk:5181/drill/61-demo-drillbits',["mapr", "maprmapr"],driver_jar)
+print("connected")
+
+curs = conn.cursor()
+print("open cursor")
+curs.execute("select * from cp.`employee.json` limit 10")
+
+print("seleted")
+curs.fetchall()
+print("JDBC cursor fetchall method")
+print(curs.fetchall())
+
+curs.close()
+
