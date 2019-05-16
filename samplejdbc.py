0605473385c76ccbbc74a9f42baaec08cd8f6124
Kevin Sturgeon <ksturgeon@gmail.com>
2019-05-16 08:41:15 -0400

    added files

--- samplejdbc.py
+++ samplejdbc.py
@@ -3,10 +3,10 @@ jdbc_driver="org.apache.drill.jdbc.Driver"
 
 # This is the literal path for the driver on an edge/client node.  
 # The relative path is $DRILL_HOME/jars/jdbc-driver/ - which is slightly different on a cluster node vs. and edge node 
-driver_jar="/opt/mapr/drill/jars/jdbc-driver/drill-jdbc-all-1.11.0.jar"
+driver_jar="/opt/mapr/drill/drill-1.12.0/jars/jdbc-driver/drill-jdbc-all-1.12.0-mapr.jar"
 
 print("connecting")
-conn = jaydebeapi.connect(jdbc_driver,'jdbc:drill:drillbit=hostname:3010',["username", "password"],driver_jar)
+conn = jaydebeapi.connect(jdbc_driver,'jdbc:drill:drillbit=10.250.101.72:31010',["mapr", "maprmapr"],driver_jar)
 print("connected")
 
 curs = conn.cursor()
@@ -16,6 +16,8 @@ curs.execute("select * from cp.`employee.json` limit 10")
 print("seleted")
 curs.fetchall()
 print("FETCHED")
+
+print curs.description
 
 curs.close()
 
--- simbajdbc.py
+++ simbajdbc.py
@@ -2,13 +2,15 @@ import jpype
 import jaydebeapi
 import os
 
-driver_dir="/home/mapr/drill-jdbc/"
+driver_dir="/opt/mapr/drill/lib/"
 classpath = str.join(":", [driver_dir+name for name in os.listdir(driver_dir)])
 
 args = "-Djava.class.path=%s" % classpath
 jpype.startJVM(jpype.getDefaultJVMPath(), args)
 
-conn = jaydebeapi.connect('com.mapr.drill.jdbc41.Driver', 'jdbc:drill:drillbit=10.10.72.78;AuthMech=MapRSASL')
+conn = jaydebeapi.connect('com.mapr.drill.jdbc41.Driver', 'jdbc:drill:drillbit=10.250.101.13:31010',["mapr","maprmapr"])
 curs = conn.cursor()
-curs.execute("select * from sys.version")
+#curs.execute("select * from sys.version")
+curs.execute("select * from cp.`employee.json` limit 10")
 curs.fetchall()
+print(curs.description)
